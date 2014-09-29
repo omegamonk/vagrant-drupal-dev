a902ea308a59afb827cffa0cf10e4c01280012cf
michfuer <mike.fuerstnau@commerceguys.com>
2014-09-29 18:14:45 -0400

    Updated build script

--- build.sh
+++ build.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
-echo -n "Enter profile name > "
-read profile
+##### Functions
 
-cat << _EOF_ > site.make
-api = 2
-core = 7.x
+# Creates a default drush makefile.
+create_site_make()
+{
+	cat <<- _EOF_ > site.make
+	api = 2
+	core = 7.x
 
-projects[drupal][type] = core
-projects[drupal][version] = "7.31"
+	projects[drupal][type] = core
+	projects[drupal][version] = "7.31"
 
-projects[$profile][type] = profile
-projects[$profile][download][type] = git
-projects[$profile][download][url] = "https://github.com/michfuer/profile_stub"
-projects[$profile][download][branch] = master
+	projects[profile_stub][type] = profile
+	projects[profile_stub][download][type] = git
+	projects[profile_stub][download][url] = "https://github.com/michfuer/profile_stub"
+	projects[profile_stub][download][branch] = master
 
-; Useful tools  ================================================================
-projects[tools][type] = module
-projects[tools][subdir] = contrib
-projects[tools][download][type] = "git"
-projects[tools][download][url] = "https://github.com/michfuer/tools"
-projects[tools][download][branch] = "master"
-_EOF_
+	# Useful tools  ================================================================
+	projects[tools][type] = module
+	projects[tools][subdir] = contrib
+	projects[tools][download][type] = "git"
+	projects[tools][download][url] = "https://github.com/michfuer/tools"
+	projects[tools][download][branch] = "master"
+	_EOF_
+}
 
-drush make --working-copy site.make site-root
+if [[ -f site.make ]]; then
+    echo "Overwrite existing site.make file (y/n)? "
+    read overwrite
+    if [[ "$overwrite" = "y" ]]; then
+    	create_site_make
+    fi
+else
+	create_site_make
+fi
+
+echo -n "Build site.make (y/n)? "
+read build
+
+if [[ "$build" = "y" ]]; then
+	drush make --working-copy site.make site-root
+fi
