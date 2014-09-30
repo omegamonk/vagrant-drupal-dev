b267c460fa9b3f1c292121e58324b70e5d1c8f82
michfuer <mike.fuerstnau@commerceguys.com>
2014-09-30 11:31:33 -0400

    Moved Vagrantfile creation to the build script

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 ##### Functions
 
 # Creates a default drush makefile.
-create_site_make()
-{
+create_site_make() {
 	cat <<- _EOF_ > site.make
 	api = 2
 	core = 7.x
@@ -19,6 +18,63 @@ create_site_make()
 	_EOF_
 }
 
+# Creates a default Vagrantfile.
+create_vagrantfile() {
+
+	echo -n "Enter desired IP address e.g. 192.168.227.x. Remember to update /etc/hosts with the same address. > "
+	read address
+
+	cat <<- _EOF_ > Vagrantfile
+	# -*- mode: ruby -*-
+	# vi: set ft=ruby :
+
+	# Notes::
+	#   Puppet for configuration.
+
+	# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+	VAGRANTFILE_API_VERSION = '2'
+
+	Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+	  config.vm.box = 'ubuntu/trusty64'
+	  config.vm.box_url = 'https://vagrantcloud.com/ubuntu/trusty64'
+
+	  # This will likely have to be more specific for each host
+	  config.vm.provision 'puppet' do |puppet|
+	    puppet.module_path = 'modules'
+	  end
+
+	  config.vm.define 'dev' do |dev|
+	    dev.vm.hostname = 'dev'
+	    dev.vm.network "private_network", ip: $address
+
+	    # Need to add the appropriate folders to access htdocs, etc.
+	    dev.vm.synced_folder 'site-root', '/app/drupal', type: 'nfs'
+	  end
+
+	  config.vm.provider 'virtualbox' do |vb|
+	    # Specify the amount of memory in MB
+	    vb.memory = 1024
+	    # Specify the number of CPUs
+	    vb.cpus = 2
+	  end
+	end
+	_EOF_
+
+}
+
+# Create/Update the Vagrantfile
+if [[ -f Vagrantfile ]]; then
+    echo -n "Overwrite existing Vagrantfile (y/n)? "
+    read overwrite
+    if [[ "$overwrite" = "y" ]]; then
+    	create_vagrantfile
+    fi
+else
+	create_vagrantfile
+fi
+
+# Create/Update the site makefile
 if [[ -f site.make ]]; then
     echo -n "Overwrite existing site.make file (y/n)? "
     read overwrite
